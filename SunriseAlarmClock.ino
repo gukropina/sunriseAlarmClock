d86b793170ba087ab642b5b935c5592beacfff2e
gukropina <gukropina@gmail.com>
2024-02-02 21:36:00 -0800

    Update SunriseAlarmClock.ino
    Tested how the arduino millis() function works vs the clock on my computer, and built the hardware and attached to the device. Tested everything, and it seems to be working well. It would be nice to have a way to set the clock that is easier, but for now you can program the time and then reset the arduino at the correct time to make sure the time is synced.

--- SunriseAlarmClock.ino
+++ SunriseAlarmClock.ino
@@ -1,31 +1,44 @@
 /*
   Turns on an RGB strip at a set time, then turns it off.
   Displays the time on an OLED screen (using a 24 hour clock)
+  test: using Arduino Uno
+        started at 6:14:38 on the arduino, and the clock on my computer was 6:14:40. They are about 2 seconds apart
+        at that time. They were 9 seconds off by 8:06:18. So in about 2 hours, it drifted to being 7 more seconds slow 
+        (the computer clock was 9-10 seconds faster than what the arduino was saying, so the arduino clock runs slower)
+        That means that about every day, it would be 84 seconds, or about a minute and a third slower. 
+        So, I could try to compensate if I want. Every hour, I could add 3-4 seconds, making the clock run 
+        a little faster, and a little closer to real time. (right now, I need to add 3.5 seconds, if my math is right)
 
 */
 
 #define RGB_LED_Red 6
 #define RGB_LED_Green 5
 #define RGB_LeD_Blue 3
+#define SERIAL_DEBUG 0      //if serial debug is 1, you will send information to the serial port
 
-int SECONDS = 55;
+#define LIGHT_ON_DELAY 200 //this is the delay that the code takes when bringing the lights on from nothing
+                           //this delay will happen 765 times when turning on the lights (one for each color,
+                           //and each color goes from 1 to 255) With it set to 100, it will take 7.65 seconds to turn
+                           //all the way on, going through each color (red, green, then blue)
+
+int SECONDS = 59;
 int MINUTES = 29;
-int HOURS = 7;
+int HOURS = 21;
 
 //this is when the alarm turns on
-int ALARM_HOURS = 7;
-int ALARM_MINUTES = 30;
+int ALARM_HOURS = 8;
+int ALARM_MINUTES = 00;
 
 //this is when the alarm turns off
-int ALARM_OFF_HOURS = 7;
-int ALARM_OFF_MINUTES = 50;
-
-int LIGHTS_ON = 0;  //lights start off
-
+int ALARM_OFF_HOURS = 8;
+int ALARM_OFF_MINUTES = 15;
 
 //things to keep track of time
 #define ONE_SECOND 1000
 unsigned long TIME_CHECK = 0;
+#define SECONDS_TO_ADD_EACH_HOUR 3   //from empirical testing, the ardiuno runs about 3-4 seconds slower per hour than 
+                                     //my PC's clock. So, I add 3 seconds every hour to the time to try to be more
+                                     //accurate
 
 
 //OLED stuff
@@ -40,6 +53,12 @@ void setup() {
   pinMode(RGB_LED_Red, OUTPUT);
   pinMode(RGB_LED_Green, OUTPUT);
   pinMode(RGB_LeD_Blue, OUTPUT);
+  
+  #if SERIAL_DEBUG
+  //if you are doing serial debugging, turn on Serial
+  Serial.begin(115200);
+  Serial.println("Hi");
+  #endif
 }
 
 // the loop function runs over and over again forever
@@ -58,12 +77,23 @@ void loop() {
     if(MINUTES==60){
       MINUTES=0;
       HOURS++;
+      SECONDS = SECONDS_TO_ADD_EACH_HOUR;  //add a few seconds, since the arduino clock runs slow
       }
     if(HOURS==24){
-      HOURS=1;
+      HOURS=0;
       } 
 
-    if(update_clock == 1){
+    //check if you need to send information to Serial port
+    #if SERIAL_DEBUG
+    Serial.print("Time: ");
+    Serial.print(HOURS);
+    Serial.print(":");
+    Serial.print(MINUTES);
+    Serial.print(":");
+    Serial.println(SECONDS);
+    #endif
+
+    if(update_clock == 1){  
       //now, update the OLED
       //update screen if it's been a second
       ssd1306_clearScreen( );
@@ -84,11 +114,12 @@ void loop() {
 
       //check the alarm
       if( (ALARM_HOURS == HOURS) && (ALARM_MINUTES == MINUTES) ){
-        //if the HOURS, MINUTES, and AM/PM are correct, turn on the light!
+        //if the HOURS and MINUTES are correct, turn on the light!
         turn_on_lights();
       }
+      //check to turn the alarm off
       if( (ALARM_OFF_HOURS == HOURS) && (ALARM_OFF_MINUTES == MINUTES) ){
-        //turn off the light an hour later
+        //turn off the light when it's programmed to turn off
         turn_off_lights();
       }
     }
@@ -104,6 +135,19 @@ void loop() {
  */
 
  void turn_on_lights( void ){
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(RGB_LED_Red, i);
+    delay(LIGHT_ON_DELAY);
+  }
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(RGB_LED_Green, i);
+    delay(LIGHT_ON_DELAY);
+  }
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(RGB_LeD_Blue, i);
+    delay(LIGHT_ON_DELAY);
+  }
+  
   digitalWrite(RGB_LED_Red, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(RGB_LED_Green, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(RGB_LeD_Blue, HIGH);   // turn the LED on (HIGH is the voltage level) 
